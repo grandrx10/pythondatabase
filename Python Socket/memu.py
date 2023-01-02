34eee1952be6cbb9cc8c98a90e42ec503ae34a06
Xuderek01 <epnightmare02@gmail.com>
2023-01-01 23:35:24 -0500

    Initialized Modules

--- Python Socket/memu.py
+++
@@ -1,3 +0,0 @@
-import pygame as pg
-from OpenGL.GL import *
-
---
+++ Python Socket/menu.py
@@ -0,0 +1,7 @@
+import pygame as pg
+import numpy as np
+from OpenGL.GL import *
+
+class menu():
+    def __init__(self):
+        pg.init()
